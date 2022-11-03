57afe83fe7b5fce807e81fe3dbc22765d59cacf8
rmastudillo <you@example.com>
2022-11-03 17:04:55 -0300

    generador

---
+++ GeneradorNumerosAleatorios.cs
@@ -0,0 +1,9 @@
+namespace Escoba;
+
+public static class GeneradorNumerosAleatorios
+{
+    private const int RandomSeed = 0;
+    private static Random rng = new Random(RandomSeed);
+
+    public static double Generar() => rng.Next();
+}
