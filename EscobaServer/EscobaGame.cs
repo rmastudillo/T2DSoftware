b76af3c859bfc8589f0123872404825de5dcb69e
Felipe <fplois@uc.cl>
2022-11-13 19:06:06 -0300

    No spaces

--- EscobaServer/EscobaGame.cs
+++ EscobaServer/EscobaGame.cs
@@ -1,5 +1,4 @@
 using static System.Int32;
-
 namespace EscobaServer;
 public class EscobaGame
 {
@@ -18,7 +17,6 @@ public class EscobaGame
     private void StartGame()
     {
         Messages.WelcomeMessage();
-
     }
 
     public void NewHand()
@@ -26,7 +24,7 @@ public class EscobaGame
         StartGame();
         GameFlow();
     }
-    
+
     private void DealingCardsToPlayers()
     {
         for (var i = 0; i < 3; i++)
@@ -41,14 +39,14 @@ public class EscobaGame
         for (var i = 0; i < 4; i++)
         {
             Board.AddCard(Board.Deck.GetTopCard());
-        } 
+        }
     }
     private void GameFlow()
     {
         while (!ThereIsAWinner())
         {
             Board.Deck.ShuffleDeck();
-            while (Board.Deck.NumberOfCards()>0)
+            while (Board.Deck.NumberOfCards() > 0)
             {
                 Messages.DealingCards(CurrentPlayer.ToString());
                 DealingCardsToPlayers();
@@ -65,13 +63,12 @@ public class EscobaGame
             RePopulateDeck();
             SwapPlayerTurn();
         }
-
         AnnounceWinner();
     }
 
     private void AnnounceWinner()
     {
-        var winnerMessage = new List<string>(){"x Fin de la partida x\n"};
+        var winnerMessage = new List<string>() { "x Fin de la partida x\n" };
         var playerOnePoints = Board.PlayerOne.GetEarnedPoints();
         var playerTwoPoints = Board.PlayerTwo.GetEarnedPoints();
         var differenceOfPoints = playerOnePoints - playerTwoPoints;
@@ -100,15 +97,13 @@ public class EscobaGame
     {
         return Board.PlayerOne.GetEarnedPoints() >= 16 || Board.PlayerTwo.GetEarnedPoints() >= 16;
     }
-
     private void ReportEndOfTurn()
     {
-        var playerOneEarnedCards = ListOfCardsToString( Board.PlayerOne.EarnedCards);
-        var playerTwoEarnedCards = ListOfCardsToString( Board.PlayerTwo.EarnedCards);
+        var playerOneEarnedCards = ListOfCardsToString(Board.PlayerOne.EarnedCards);
+        var playerTwoEarnedCards = ListOfCardsToString(Board.PlayerTwo.EarnedCards);
         var points = new List<int>() { Board.PlayerOne.GetEarnedPoints(), Board.PlayerTwo.GetEarnedPoints() };
         Messages.EndTurnReport(playerOneEarnedCards, playerTwoEarnedCards, points);
     }
-
     private void SwapPlayerTurn()
     {
         CurrentPlayer = (CurrentPlayer == Board.PlayerOne) ? Board.PlayerTwo : Board.PlayerOne;
@@ -119,15 +114,14 @@ public class EscobaGame
         var playerInput = GetPlayerInput(CurrentPlayer._hand.Count);
         PlayACardFromHand(playerInput);
     }
-
     private void PlayACardFromHand(int playerInput)
     {
         var cardToPlay = CurrentPlayer.PlayCardFromHand(playerInput);
-        var possiblePlays= CheckPosiblePlays(cardToPlay).ToList();
+        var possiblePlays = CheckPosiblePlays(cardToPlay).ToList();
         switch (possiblePlays.Count)
         {
             case 0:
-                Board.AddCard(cardToPlay) ;
+                Board.AddCard(cardToPlay);
                 break;
             case 1:
                 MakingAPlay(possiblePlays[0]);
@@ -137,39 +131,34 @@ public class EscobaGame
                 break;
         }
     }
-    
-
     private void AsignPlayersPoints()
     {
-  
         CheckForSevenGoldCard();
         CheckForTheMostSevensEarned();
         CheckForTheMostNumberOfCardsEarned();
         CheckForTheMostGoldCardsEarned();
-
     }
     private void CheckForTheMostSevensEarned()
     {
         var playerOneEarnedCardsCount = Board.PlayerOne.GetNumberOfCardWithSevenEarned();
         var playerTwoEarnedCardsCount = Board.PlayerTwo.GetNumberOfCardWithSevenEarned();
-        CheckForNumberOfCardsEarned(playerOneEarnedCardsCount,playerTwoEarnedCardsCount);
+        CheckForNumberOfCardsEarned(playerOneEarnedCardsCount, playerTwoEarnedCardsCount);
     }
     private void CheckForTheMostGoldCardsEarned()
     {
         var playerOneEarnedCardsCount = Board.PlayerOne.GetNumberOfGoldCardsEarned();
         var playerTwoEarnedCardsCount = Board.PlayerTwo.GetNumberOfGoldCardsEarned();
-        CheckForNumberOfCardsEarned(playerOneEarnedCardsCount,playerTwoEarnedCardsCount);
+        CheckForNumberOfCardsEarned(playerOneEarnedCardsCount, playerTwoEarnedCardsCount);
     }
-
     private void CheckForTheMostNumberOfCardsEarned()
     {
         var playerOneEarnedCardsCount = Board.PlayerOne.GetNumberOfCardsEarned();
         var playerTwoEarnedCardsCount = Board.PlayerTwo.GetNumberOfCardsEarned();
-        CheckForNumberOfCardsEarned(playerOneEarnedCardsCount,playerTwoEarnedCardsCount);
+        CheckForNumberOfCardsEarned(playerOneEarnedCardsCount, playerTwoEarnedCardsCount);
     }
-    private void CheckForNumberOfCardsEarned(int playerOneNumberOfCards,int playerTwoNumberOfCards)
+    private void CheckForNumberOfCardsEarned(int playerOneNumberOfCards, int playerTwoNumberOfCards)
     {
-        var differenceInNumberOfCards = playerOneNumberOfCards -playerTwoNumberOfCards;
+        var differenceInNumberOfCards = playerOneNumberOfCards - playerTwoNumberOfCards;
         switch (differenceInNumberOfCards)
         {
             case 0:
@@ -183,19 +172,17 @@ public class EscobaGame
                 Board.PlayerTwo.AddAPoint();
                 break;
         }
-        
     }
-
     private void CheckForSevenGoldCard()
     {
         if (Board.PlayerOne.CheckForTheSevenGoldCard())
         {
             Board.PlayerOne.AddAPoint();
         }
-        else if(Board.PlayerTwo.CheckForTheSevenGoldCard())
+        else if (Board.PlayerTwo.CheckForTheSevenGoldCard())
         {
             Board.PlayerTwo.AddAPoint();
-        }  
+        }
     }
     private void MakingAPlay(List<Card> cards)
     {
@@ -205,32 +192,27 @@ public class EscobaGame
             Board.RemoveCard(card);
         }
         CurrentPlayer.AddEarnedCards(cards);
-        Messages.CardWon(CurrentPlayer.ToString(),ListOfCardsToString(cards));
-        if (Board.CardsOnTable.Count == 0)Escoba();
+        Messages.CardWon(CurrentPlayer.ToString(), ListOfCardsToString(cards));
+        if (Board.CardsOnTable.Count == 0) Escoba();
     }
-
     private void PlayerGetBoardCards()
     {
         var endTurnEarnedCards = new List<Card>(Board.CardsOnTable);
         foreach (var card in endTurnEarnedCards)
         {
             Board.RemoveCard(card);
-            
         }
         LastPlayerThatMakeAPlay.AddEarnedCards(endTurnEarnedCards);
     }
-
     private void Escoba()
     {
         CurrentPlayer.AddAPoint();
         Messages.Escoba(CurrentPlayer.ToString());
     }
-
     private List<string> ListOfCardsToString(List<Card> possiblePlay)
     {
         return possiblePlay.Select(card => card.ToString()).ToList();
     }
-
     private List<List<string>> ListOfPlaysToString(List<List<Card>> possiblePlays)
     {
         return possiblePlays.Select(ListOfCardsToString).ToList();
@@ -242,47 +224,48 @@ public class EscobaGame
         var playerInput = GetPlayerInput(ListOfPlaysToString(possiblePlays).Count);
         MakingAPlay(possiblePlays[playerInput]);
     }
-
     private void ShowPlayerOptionToPlay()
     {
         var playerName = CurrentPlayer.ToString();
         var hand = CurrentPlayer.PlayerHandToString();
         var cardsOnTable = Board.CardsOnTableToString();
-        Messages.MainTurn(playerName,hand,cardsOnTable);
+        Messages.MainTurn(playerName, hand, cardsOnTable);
     }
-
     private int GetPlayerInput(int maxInputValue)
     {
         var inputIsInt = TryParse(Console.ReadLine(), out var playerValidInput);
-        while (!inputIsInt || ( playerValidInput<1) || (playerValidInput>maxInputValue))
+        while (!inputIsInt || (playerValidInput < 1) || (playerValidInput > maxInputValue))
         {
             Messages.InvalidInput();
-            inputIsInt = TryParse(Console.ReadLine(), out  playerValidInput);
+            inputIsInt = TryParse(Console.ReadLine(), out playerValidInput);
         }
-        return playerValidInput - 1 ;
+        return playerValidInput - 1;
     }
-
     private IEnumerable<List<Card>> CheckPosiblePlays(Card cardToPlay)
     {
         var possibleCardsToCombine = new List<Card>(Board.CardsOnTable);
-        possibleCardsToCombine.Insert(0,cardToPlay);
+        possibleCardsToCombine.Insert(0, cardToPlay);
         return GetPlays(possibleCardsToCombine, 15, new List<Card>());
     }
-    
-    
-    public  IEnumerable<List<Card>> GetPlays(List<Card> cardsToCombine, int targetSumOfCards, List<Card> listOfPlays) {
-        for (var card = 0; card < cardsToCombine.Count; card++) {
+    public IEnumerable<List<Card>> GetPlays(List<Card> cardsToCombine, int targetSumOfCards, List<Card> listOfPlays)
+    {
+        for (var card = 0; card < cardsToCombine.Count; card++)
+        {
             var currentSum = targetSumOfCards - cardsToCombine[card].Value;
-            var validPlays = new List<Card>(){cardsToCombine[card]};
+            var validPlays = new List<Card>() { cardsToCombine[card] };
             validPlays.AddRange(listOfPlays);
-            if (currentSum == 0) {
+            if (currentSum == 0)
+            {
                 if (validPlays.First() == cardsToCombine.First()) yield return validPlays;
-            } else {
+            }
+            else
+            {
                 var possiblePlays = new List<Card>(cardsToCombine.Take(card).Where(
                         possibleCard => possibleCard.Value <= targetSumOfCards));
                 if (possiblePlays.Count <= 0) continue;
                 foreach (var plays in GetPlays(
-                             possiblePlays, currentSum, validPlays)) {
+                             possiblePlays, currentSum, validPlays))
+                {
                     yield return plays;
                 }
             }
